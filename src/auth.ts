67325c4f82134b479a9d69ff98d75236ea8aa684
Muhd-Saifullah-dev <saifullahdev98@gmail.com>
2025-12-03 10:41:49 -0700

    adding google signin and signup button

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import NextAuth from "next-auth"
 import connectDb from "./lib/db"
 import User from "./model/user.model"
 import bcrypt from "bcryptjs"
+import Google from "next-auth/providers/google"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
    Credentials({
@@ -30,6 +31,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         }
       },
     }),
+    Google({
+      clientId:process.env.CLIENT_ID,
+      clientSecret:process.env.CLIENT_SECRET
+    })
   ],
   callbacks:{
     jwt({token,user}) {
