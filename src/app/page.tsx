4886b660e3908ab3ebfaf210093f167137da7b7a
Muhd-Saifullah-dev <saifullahdev98@gmail.com>
2025-12-07 08:53:41 -0700

    adding hero section in user dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,42 @@
-import { auth } from "@/auth";
-import EditRoleMobile from "@/components/EditRoleMobile";
-import Nav from "@/components/Nav";
-import connectDb from "@/lib/db";
-import User from "@/model/user.model";
-import { redirect } from "next/navigation";
-
+import { auth } from '@/auth';
+import AdminDashboard from '@/components/AdminDashboard';
+import DeliveryBoyDashboard from '@/components/DeliveryBoyDashboard';
+import EditRoleMobile from '@/components/EditRoleMobile';
+import Nav from '@/components/Nav';
+import UserDashboard from '@/components/UserDashboard';
+import connectDb from '@/lib/db';
+import User from '@/model/user.model';
+import { redirect } from 'next/navigation';
 
 async function Home() {
-await connectDb()
-const session=await auth()
-console.log("session ",session)
-const user=await User.findById(session?.user?.id)
-if(!user){
-  redirect("/login")
-}
+  await connectDb();
+  const session = await auth();
+  console.log('session ', session);
+  const user = await User.findById(session?.user?.id);
+  if (!user) {
+    redirect('/login');
+  }
 
-const inComplete=!user.mobile || !user.role ||(!user.mobile && user.role=="user")
+  const inComplete =
+    !user.mobile || !user.role || (!user.mobile && user.role == 'user');
 
-if(inComplete){
-return <EditRoleMobile/>
-}
+  if (inComplete) {
+    return <EditRoleMobile />;
+  }
 
-const plainUser=JSON.parse(JSON.stringify(user))
+  const plainUser = JSON.parse(JSON.stringify(user));
   return (
     <>
-    <Nav user={plainUser}/>
+      <Nav user={plainUser} />
+      {user.role == 'user' ? (
+        <UserDashboard />
+      ) : user.role == 'admin' ? (
+        <AdminDashboard />
+      ) : (
+        <DeliveryBoyDashboard />
+      )}
     </>
   );
 }
 
-
-export default Home
+export default Home;
