aeefcd58e6b26b7ec80faa48374336a0e2bba89f
Muhd-Saifullah-dev <saifullahdev98@gmail.com>
2026-01-15 21:03:26 +0500

    adding stripe webhook

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,37 +1,42 @@
-import { getToken } from "next-auth/jwt"
-import { NextRequest,NextResponse} from "next/server"
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
 
+export async function proxy(req: NextRequest) {
+    const { pathname } = req.nextUrl;
+    // console.log(pathname)
+    const publicRoutes = ['/login', '/register', '/api/auth'];
+    if (publicRoutes.some((path) => pathname.startsWith(path))) {
+        return NextResponse.next();
+    }
+    const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+    console.log('token is here', token);
+    console.log('req url ', req.url);
+    if (!token) {
+        const loginUrl = new URL('/login', req.url);
+        loginUrl.searchParams.set('callback', req.url);
+        // console.log("login url ",loginUrl)
+        return NextResponse.redirect(loginUrl);
+    }
 
-export async function proxy(req:NextRequest) {
-  const {pathname}=req.nextUrl
-  // console.log(pathname)
-  const publicRoutes=["/login","/register","/api/auth"]
-  if(publicRoutes.some((path)=>pathname.startsWith(path))){
-    return NextResponse.next()
-  }
-  const token=await getToken({req,secret:process.env.AUTH_SECRET})
-  console.log("token is here",token)
-  console.log("req url ",req.url)
-  if(!token){
-    const loginUrl=new URL("/login",req.url)
-    loginUrl.searchParams.set("callback",req.url)
-    // console.log("login url ",loginUrl)
-      return NextResponse.redirect(loginUrl)
-  }
+    const role = token.role;
+    if (pathname.startsWith('/user') && role !== 'user') {
+        return NextResponse.redirect(new URL('/unauthorized', req.url));
+    }
+    if (pathname.startsWith('/delivery') && role !== 'deliveryBoy') {
+        return NextResponse.redirect(new URL('/unauthorized', req.url));
+    }
+    if (pathname.startsWith('/admin') && role !== 'admin') {
+        return NextResponse.redirect(new URL('/unauthorized', req.url));
+    }
 
-  const role=token.role
-  if(pathname.startsWith("/user") && role!=="user"){
-    return NextResponse.redirect(new URL("/unauthorized",req.url))
-  } 
-   if(pathname.startsWith("/delivery") && role!=="deliveryBoy"){
-    return NextResponse.redirect(new URL("/unauthorized",req.url))
-  }  if(pathname.startsWith("/admin") && role!=="admin"){
-    return NextResponse.redirect(new URL("/unauthorized",req.url))
-  }
-
-  return NextResponse.next()
+    return NextResponse.next();
 }
 
-export const config={
-  matcher:'/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-}
+export const config = {
+    matcher: [
+        /*
+      Include everything except static, images, favicon and stripe webhook
+    */
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|api/user/stripe/webhook).*)',
+    ],
+};
