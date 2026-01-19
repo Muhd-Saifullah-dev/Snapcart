78ac218c8f9568bd40c9768af74483bb825b5d0c
Muhd-Saifullah-dev <saifullahdev98@gmail.com>
2026-01-19 18:09:58 +0500

    add deliveryAssignment model and update order and adding location in user model

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,46 +1,68 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 interface IUSER {
-    _id?: mongoose.Schema.Types.ObjectId
-    name: string
-    email: string
-    password?: string
-    mobile?: string
-    role: "user" | "deliveryBoy" | "admin"
-    image?:string
+    _id?: mongoose.Schema.Types.ObjectId;
+    name: string;
+    email: string;
+    password?: string;
+    mobile?: string;
+    role: 'user' | 'deliveryBoy' | 'admin';
+    image?: string;
+    location: {
+        type: {
+            type: string;
+            enum: string[];
+            default: string;
+        };
+        coordinates: {
+            type: number[];
+            default: number[];
+        };
+    };
 }
 
-const userSchema = new mongoose.Schema<IUSER>({
-    name: {
-        type: String,
-        required:true
-    },
-    email: { 
-        type: String, 
-        unique: true,
-         required: true 
+const userSchema = new mongoose.Schema<IUSER>(
+    {
+        name: {
+            type: String,
+            required: true,
         },
-    password: { 
-        type: String,
-        required:false
-     },
-     mobile:{
-        type:String,
-        required:false
-     },
-     role:{
-        type:String,
-        enum:["user","deliveryBoy","admin"],
-        default:"user"
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: false,
+        },
+        mobile: {
+            type: String,
+            required: false,
+        },
+        role: {
+            type: String,
+            enum: ['user', 'deliveryBoy', 'admin'],
+            default: 'user',
+        },
+        image: {
+            type: String,
+        },
+        location: {
+            type: {
+                type: String,
+                enum: ['Point'],
+                default: 'Point',
+            },
+            coordinates: {
+                type: [Number],
+                default: [0, 0],
+            },
+        },
+    },
+    { timestamps: true }
+);
 
-     },
-     image:{
-        type:String,
-     
-     }
-
-
-}, { timestamps: true })
-
-const User=mongoose.models.User ||   mongoose.model("User",userSchema);
-export default User
+userSchema.index({ location: '2dsphere' });
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+export default User;
